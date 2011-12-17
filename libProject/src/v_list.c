8d71f9f8bc95a8c79129c3b4b6ecb0f6c725d2cc
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2011-12-17 15:11:11 +0100

    proper list type init

--- libProject/src/v_list.c
+++ libProject/src/v_list.c
@@ -4,13 +4,26 @@
 #include "v_runtime.h"
 #include "v_type.h"
 #include "v_string.h"
+#include "v_array.h"
+#include <stddef.h>
 
 void v_bootstrap_list_init_type(struct v_runtime *rt) {
-    rt->built_in_types.list->fields = NULL;
+    v_field **fields;
+    rt->built_in_types.list->fields = v_bootstrap_type_create_field_array(rt, 2);
     rt->built_in_types.list->kind = V_T_OBJECT;
     rt->built_in_types.list->name = v_bootstrap_string_create("AnyList");
     rt->built_in_types.list->numFields = 0;
     rt->built_in_types.list->size = sizeof(v_list_obj);
+
+    fields = rt->built_in_types.list->fields->data;
+    
+    fields[0]->name = v_bootstrap_string_create("data");
+    fields[0]->offset = offsetof(v_list_obj, data);
+    fields[0]->type = rt->built_in_types.any;
+
+    fields[1]->name = v_bootstrap_string_create("next");
+    fields[1]->offset = offsetof(v_list_obj, next);
+    fields[1]->type = rt->built_in_types.list;
 }
 
 v_list_obj *create(v_thread_context *ctx) {
