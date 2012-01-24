53ae312756e7e3547b67d479b3c88eab6a2f3c22
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2012-01-24 05:40:27 +0100

    Make sure array data has same alignment as heap objects

--- libProject/src/v_array.h
+++ libProject/src/v_array.h
@@ -4,9 +4,16 @@
 #include "../../platformProject/src/v_platform.h"
 #include "v_typedefs.h"
 
+/* For now the alignment of the array data is hardcoded to be 16 bytes
+ because the heap data alignment is 16 bytes and we use padding in the
+ struct to make the data pointer 16 byte aligned as well.
+ TODO: user specified alignment of the data field. */
 struct vArray {
     vTypeRef element_type;
     uword num_elements;
+#ifndef VLANG64
+    uword padding[2];
+#endif
     char data[0]; /* Variable size */
 };
 
