4d752c6661083ef7463f27ad161cbec7cbd70967
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2012-03-23 06:25:56 +0100

    create signature

--- libProject/src/o_function.c
+++ libProject/src/o_function.c
@@ -1,10 +1,22 @@
 #include "o_function.h"
-
+#include "o_memory.h"
+#include "o_thread_context.h"
+#include "o_runtime.h"
 
 oParameterRef _oParameterCreate(oThreadContextRef ctx, oStringRef name, oTypeRef type) {
 }
 
 oSignatureRef _oSignatureCreate(oThreadContextRef ctx, oTypeRef returnType, oArrayRef parameters) {
+    oROOTS(ctx)
+    oSignatureRef sig;
+    oENDROOTS
+    
+    oRoots.sig = (oSignatureRef)oHeapAlloc(ctx->runtime->builtInTypes.signature);
+    oRoots.sig->retType = returnType;
+    oRoots.sig->parameters = oHeapCopyObjectShared(parameters);
+    oRETURN(oRoots.sig);
+
+    oENDFN(oSignatureRef)
 }
 
 o_bool oSignatureEquals(oThreadContextRef ctx,
