4a7c3d6be57f89e144054d9a696ccd34c0f4a4b4
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2012-06-17 15:39:16 +0200

    add type test

--- testProject/src/box_tests.c
+++ testProject/src/box_tests.c
@@ -5,32 +5,6 @@
 #include <assert.h>
 #include <stdlib.h>
 
-//static o_bool BoxCheckArrayBit(Box* box);
-//static o_bool BoxCheckGCMarkedBit(Box* box);
-//static o_bool BoxCheckSharedBit(Box* box);
-//
-//static void BoxSetArrayBit(Box* box);
-//static void BoxSetGCMarkedBit(Box* box);
-//static void BoxSetSharedBit(Box* box);
-//
-//static void BoxClearArrayBit(Box* box);
-//static void BoxClearGCMarkedBit(Box* box);
-//static void BoxClearSharedBit(Box* box);
-//
-//struct Type;
-//
-//static struct Type* BoxGetType(Box* box);
-//static void BoxSetType(Box* box, struct Type* type);
-//
-//static ArrayInfo* BoxGetArrayInfo(Box* box);
-//
-//static pointer BoxGetObject(Box* box);
-//
-//static Box* BoxGetBox(pointer object);
-//
-//static uword BoxCalcObjectBoxSize(uword type_size);
-//static uword BoxCalcArrayBoxSize(uword type_size, uword alignment, uword num_elements);
-
 static void correct_size() {
     uword objSize = 80;
     uword arrSize = 100;
@@ -111,6 +85,7 @@ static void storeObject() {
     box = malloc(BoxCalcObjectBoxSize(sizeof(testStruct)));
     
     tsp = BoxGetObject(box);
+    BoxSetType(box, (Type*)box);
     (*tsp) = ts;
     
     box2 = box;
@@ -125,6 +100,7 @@ static void storeObject() {
     assert(ts.two = tsp->two);
     assert(ts.p == tsp->p);
     assert(tsp->p == &ts);
+    assert(BoxGetType(box2) == (Type*)box);
     
     free(box);
 }
