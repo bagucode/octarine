82a740f01dd24f6d9161752177cfbe4a8ad14fd0
Markus Gustavsson <markus.gustavsson@gmail.com>
2012-07-12 12:51:11 +0200

    string->symbol

---
+++ libProject/src/symbol.h
@@ -0,0 +1,20 @@
+#ifndef octarine_symbol_h
+#define octarine_symbol_h
+
+#include "basic_types.h"
+
+typedef struct Symbol {
+    u8* characters;
+} Symbol;
+
+struct OctHeap;
+
+static void SymbolCreate(Symbol* sym, const char* cstr, struct OctHeap* heap);
+
+static void _SymbolCreate(Symbol* sym, const char* cstr, u8* boxedArray);
+
+static o_bool SymbolEquals(Symbol* sym1, Symbol* sym2);
+
+static uword SymbolHash(Symbol* sym);
+
+#endif
---
+++ libProject/src/symbol_impl.h
@@ -0,0 +1,18 @@
+#ifndef octarine_oct_string_impl_h
+#define octarine_oct_string_impl_h
+
+#include "symbol.h"
+#include "heap.h"
+
+static void SymbolCreate(Symbol* sym, const char* cstr, OctHeap* heap) {
+
+}
+
+static o_bool SymbolEquals(Symbol* sym1, Symbol* sym2) {
+}
+
+static uword SymbolHash(Symbol* sym) {
+}
+
+
+#endif
